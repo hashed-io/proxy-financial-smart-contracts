4bb69433711582de1ec3629fb8627efa21d2e1b1
tlacloc <desneruda@gmail.com>
2022-04-30 01:32:11 -0500

    complete unssign function for users

--- src/users/base_user.cpp
+++ src/users/base_user.cpp
@@ -58,7 +58,19 @@ void User::unassign(const eosio::name &account, const uint64_t &project_id)
   projects::user_tables user_t(contract_name, contract_name.value);
   auto user_itr = user_t.find(account.value);
 
-  // TODO check if user has the project assigned
+  bool is_assigned = false;
+  for (size_t i = 0; i < user_itr->related_projects.size(); i++)
+  {
+    if(user_itr->related_projects[i] == project_id) {
+      is_assigned = true;
+    }
+  }
+
+  check(!is_assigned, "User is not assigned to this project!");
+
+  user_t.modify(user_itr, contract_name, [&](auto & item){
+    item.related_projects.erase(std::remove(item.related_projects.begin(), item.related_projects.end(), project_id), item.related_projects.end());
+  });
 
 }
 
