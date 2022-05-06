e5bc22cb37ccc0a39ddaea42581ae1edaa52c287
didiermis <didier.mis.may@gmail.com>
2022-05-06 16:14:27 -0500

    add check if exists previous regional center

--- src/users/regional_center_user.cpp
+++ src/users/regional_center_user.cpp
@@ -20,16 +20,14 @@ void RegionalCenter::assign_impl(const eosio::name &account, const uint64_t &pro
   projects::project_tables project_t(contract_name, contract_name.value);
   auto project_itr = project_t.find(project_id);
 
+  //TODO: check if there is a prev regional center assigned to the project
+  // review statement
+  check(project_itr -> builder.size() == 0, "There can only be one Regional Center per project!")
   project_t.modify(project_itr, contract_name, [&](auto &item)
-                   { item.regional_center.push_back(account); });
+                   { item.regional_center = account; });
   
   projects::user_tables user_t(contract_name, contract_name.value);
   auto user_itr = user_t.find(account.value);
-  //TODO: check if there is a prev regional center assigned to the project
-  //update: project can have multiple regional_center, check is not required
-
-  // review statement: Regional Center can have only one project
-  check(user_itr->related_projects.size() < 1, "Regional Center can only manage one project!");
 
   user_t.modify(user_itr, contract_name, [&](auto &item)
                 { item.related_projects.push_back(project_id); });
