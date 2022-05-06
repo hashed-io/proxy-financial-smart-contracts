0c57577cc167409d747d9eef03b741525d2ac9c9
tlacloc <desneruda@gmail.com>
2022-05-06 12:38:45 -0500

    scripts to manage deploy and actions on cleos

--- docs/cleos/testnet/reset.sh
+++ docs/cleos/testnet/reset.sh
@@ -10,16 +10,9 @@ echo 'Reset of: proxyv2prmss'
 cleos -u https://test.telos.kitchen push action proxyv2prmss reset '{}' -p proxyv2prmss@active
 
 
-echo 'Reset of: proxyv2prjct'
+echo 'Reset of: proxyv3prjct'
 cleos -u https://test.telos.kitchen push action proxyv3prjct reset '{}' -p proxyv3prjct@active
 
 
-echo 'Reset of: proxyv2trnsc'
+echo 'Reset of: proxyv3trnsc'
 cleos -u https://test.telos.kitchen push action proxyv3trnsc reset '{}' -p proxyv3trnsc@active
-
-
-# echo 'Init projects and accounts'
-
-cleos -u https://test.telos.kitchen push action proxyv3prjct init '{}' -p proxyv3prjct@active
-
-cleos -u https://test.telos.kitchen push action proxyv2accnt init '{}' -p proxyv2accnt@active
