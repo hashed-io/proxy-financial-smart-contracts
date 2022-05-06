ded6068364172fd221d2259825bd9bff2df43840
didiermis <didier.mis.may@gmail.com>
2022-05-06 16:12:41 -0500

    Remove extra spaces

--- src/users/builder_user.cpp
+++ src/users/builder_user.cpp
@@ -25,7 +25,6 @@ void Builder::assign_impl(const eosio::name &account, const uint64_t &project_id
   // review statement
   check(project_itr -> builder.size() == 0, "There can only be one builder per project!")
   //check(project_itr -> builder.empty(), "There can only be one builder per project!")
-
   project_t.modify(project_itr, contract_name, [&](auto &item)
                    { item.builder = account; });
 
