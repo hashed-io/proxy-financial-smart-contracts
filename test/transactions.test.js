0f33e0e3681e0a2f0f502d2af6a602bb983a3576
didiermis <didier.mis.may@gmail.com>
2022-05-09 16:21:43 -0500

    merge

--- test/transactions.test.js
+++ test/transactions.test.js
@@ -347,7 +347,7 @@ describe("Tests for transactions smart contract", async function () {
   ]
 
   drawdownApproveCases.forEach(({ testName, drawdown_id }) => {
-    it.only(testName, async () => {
+    it(testName, async () => {
       // Arrange
       const transaction = await TransactionFactory.createWithDefaults({});
       // console.log(...transaction.getCreateParams());
@@ -425,7 +425,7 @@ describe("Tests for transactions smart contract", async function () {
   ]
 
   drawdownRejectCases.forEach(({ testName, drawdown_id }) => {
-    it.only(testName, async () => {
+    it(testName, async () => {
       // Arrange
       const transaction = await TransactionFactory.createWithDefaults({});
       // console.log(...transaction.getCreateParams());
