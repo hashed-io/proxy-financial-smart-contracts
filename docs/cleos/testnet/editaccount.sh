b95294312c3123bd503e78f0926a2866bcea9e6f
tlacloc <desneruda@gmail.com>
2022-05-12 19:24:32 -0500

    hotfix

---
+++ docs/cleos/testnet/editaccount.sh
@@ -0,0 +1,9 @@
+cleos -u https://testnet.telos.caleos.io push action proxyv2accnt editaccount '{
+  "actor" : "proxyadmin11",
+  "project_id" : "0",
+  "account_id" : "1",
+  "account_name" : "nombre prueba",
+  "budget_amount" : "10.00 USD",
+  "naics_code" : "411",
+  "jobs_multiplier" : "52448"
+}' -p proxyadmin11@active
